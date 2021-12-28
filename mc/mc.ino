06eaf88695dcd7cf7a8722bce0a7e7369877b472
frknilisu <frkn.ilisu@gmail.com>
2021-12-28 19:16:51 +0100

    ble msg to set points

--- mc/mc.ino
+++ mc/mc.ino
@@ -1,290 +1,193 @@
 #include <Wire.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
 #include "AS5600.h"
 #include "Point.h"
 
-#ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
-  #define SERIAL Serial
-  #define SYS_VOL   3.3
+#if CONFIG_FREERTOS_UNICORE
+#define ARDUINO_RUNNING_CORE 0
 #else
-  #define SERIAL Serial
-  #define SYS_VOL   5
+#define ARDUINO_RUNNING_CORE 1
 #endif
 
 #define SDA 21
 #define SCL 22
 
+#define joyX 2
+#define joyY 4
+#define joySW 14
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
 AMS_5600 ams5600;
 Point pA, pB;
 
-String lastResponse;
-String noMagnetStr = "Error: magnet not detected";
+String lastCout;
 
-int ang, lang = 0;
 int segmentCounter = 0;
 int homeSegment = 0;
 int previousRawSegment = 0;
 int currentRawSegment = 0;
 
+volatile bool joyPressed = false;
+bool startProgramming = false;
+bool setA = false;
+bool setB = false;
+
+enum BLEMsgsEnum {
+    msg_StartProgramming,
+    msg_StopProgramming,
+    msg_SetA,
+    msg_SetB
+};
+
+BLEMsgsEnum hashit(std::string const& inString) {
+    if (inString == "startProgramming") return BLEMsgsEnum::msg_StartProgramming;
+    if (inString == "stopProgramming") return BLEMsgsEnum::msg_StopProgramming;
+    if (inString == "setA") return BLEMsgsEnum::msg_SetA;
+    if (inString == "setB") return BLEMsgsEnum::msg_SetB;
+}
+
+/*------------------------------------------------------*/
+/*---------------------- Callbacks ---------------------*/
+/*------------------------------------------------------*/
+class MyServerCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+    Serial.println("***onConnect***");
+    //_BLEClientConnected = true;
+  }
+  
+  void onDisconnect(BLEServer* pServer) {
+    Serial.println("***onDisconnect***");
+    //_BLEClientConnected = false;
+  }
+};
+
+class MyCharacteristicCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string value = pCharacteristic->getValue();
+
+      if (value.length() > 0) {
+        Serial.println("*********");
+        Serial.print("New value: ");
+        Serial.print(value);
+        Serial.println();
+        Serial.println("*********");
+        
+        switch(hashit(value)) {
+          case BLEMsgsEnum::msg_StartProgramming:
+            Serial.println("-- startProgramming --");
+            startProgramming = true;
+            break;
+          case BLEMsgsEnum::msg_StopProgramming:
+            Serial.println("-- stopProgramming --");
+            startProgramming = false;
+            break;
+          case BLEMsgsEnum::msg_SetA:
+            Serial.println("-- setA --");
+            
+            currentRawSegment = ams5600.getRawAngle();
+            pA.setRemainder(currentRawSegment);
+            pA.setSegment(segmentCounter);
+            
+            lastCout = "Point A - Current Segment: " + String(currentRawSegment) + "\n" + \
+                       "Point A - Segment Counter: " + String(segmentCounter);
+            
+            setA = true;
+            break;
+          case msg_SetB:
+            Serial.println("-- setB --");
+            
+            currentRawSegment = ams5600.getRawAngle();
+            pB.setRemainder(currentRawSegment);
+            pB.setSegment(segmentCounter);
+            
+            lastCout = "Point B - Current Segment: " + String(currentRawSegment) + "\n" + \
+                       "Point B - Segment Counter: " + String(segmentCounter);
+            
+            setA = true;
+            break;
+        }
+      }
+    }
+};
+
+/*--------------------------------------------------------------*/
+/*---------------------- Utility Functions ---------------------*/
+/*--------------------------------------------------------------*/
+void InitBLE() {
+  BLEDevice::init("MyESP32 BLE");
+  
+  // Create the BLE Server
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create the BLE Characteristic
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+  pCharacteristic->setCallbacks(new MyCharacteristicCallbacks());
+
+  pCharacteristic->setValue("Hello World");
+
+  BLEAdvertising *pAdvertising = pServer->getAdvertising();
+  
+  pAdvertising->addServiceUUID(pService->getUUID());
+  
+  pService->start();
+  
+  // Start advertising
+  pAdvertising->start();
+}
+
+/*--------------------------------------------------*/
+/*---------------------- Tasks ---------------------*/
+/*--------------------------------------------------*/
+
+void TaskBLEAdvertise(void *pvParameters)  // This is a task.
+{
+  (void) pvParameters;
+
+  Serial.println("Starting ESP32 BLE Client application...");
+  
+  InitBLE();
+
+  for (;;) // A Task shall never return or exit.
+  {
+    //Serial.println("TaskBLEAdvertise - Loop Start ");
+    
+    //Serial.println("TaskBLEscanAndWrite - 1 Sec Task Delay");
+    vTaskDelay(1000); // Delay a second between loops.
+  }
+}
+
 void setup() {
+  Serial.begin(115200);
   Wire.begin(SDA, SCL);
-  SERIAL.begin(115200);
-  SERIAL.println(">>>>>>>>>>>>>>>>>>>>>>>>>>> ");
-  while(ams5600.detectMagnet() == 0) {
-    delay(1000);
-  }
-  if(ams5600.detectMagnet() == 1 ){
-      SERIAL.print("Current Magnitude: ");
-      SERIAL.println(ams5600.getMagnitude());
-
-      SERIAL.print("Current Raw Angle: ");
-      homeSegment = ams5600.getRawAngle();
-      SERIAL.println(String(convertRawAngleToDegrees(homeSegment), DEC));
-  }
-  else{
-      SERIAL.println("Can not detect magnet");
-  }
-  printMenu();
-}
-
-
-/*******************************************************
-/* function: printMenu
-/* In: none
-/* Out: none
-/* Description: prints menu options and result of last
-/* command
-/*******************************************************/
-void printMenu()
-{
-  for(int i=0; i<20;i++)
-    SERIAL.println();
-  SERIAL.println("AS5600 SERIAL Interface Program");
-  SERIAL.println("");
-  if(lastResponse.length()>0)
-  {
-    SERIAL.println(lastResponse);
-    SERIAL.println("");
-  }
-  SERIAL.print("1 - Set start position\t|  "); SERIAL.println(" 6 - get end position");
-  SERIAL.print("2 - Set end position\t|  ");   SERIAL.println(" 7 - get raw angle");
-  SERIAL.print("3 - Set max angle range\t|  ");  SERIAL.println(" 8 - get scaled angle");
-  SERIAL.print("4 - Get max angle range\t|  ");  SERIAL.println(" 9 - detect magnet");
-  SERIAL.print("5 - Get start position \t\t|  ");     SERIAL.println("10 - get magnet strength");
-  SERIAL.print("11 - Set PointA \t\t|  ");     SERIAL.println("12 - set PointB");
-  SERIAL.println();
-  SERIAL.print("Number of burns remaining: "); SERIAL.println(String(3 - ams5600.getBurnCount()));
-  SERIAL.println("96 - Burn Angle");
-  SERIAL.println("98 - Burn Settings (one time)");
-}
-
-/*******************************************************
-/* Function: convertRawAngleToDegrees
-/* In: angle data from AMS_5600::getRawAngle
-/* Out: human readable degrees as float
-/* Description: takes the raw angle and calculates
-/* float value in degrees.
-/*******************************************************/
-float convertRawAngleToDegrees(word newAngle)
-{
-  /* Raw data reports 0 - 4095 segments, which is 0.087 of a degree */
-  float retVal = newAngle * 0.087;
-  return retVal;
-}
-
-/*******************************************************
-/* Function: convertScaledAngleToDegrees
-/* In: angle data from AMS_5600::getScaledAngle
-/* Out: human readable degrees as float
-/* Description: takes the scaled angle and calculates
-/* float value in degrees.
-/*******************************************************/
-float convertScaledAngleToDegrees(word newAngle)
-{
-  word startPos = ams5600.getStartPosition();
-  word endPos = ams5600.getEndPosition();
-  word maxAngle = ams5600.getMaxAngle();
-
-  float multipler = 0;
-
-  /* max angle and end position are mutually exclusive*/
-  if(maxAngle >0)
-  {
-    if(startPos == 0)
-      multipler = (maxAngle*0.0878)/4096;
-    else  /*startPos is set to something*/
-      multipler = ((maxAngle*0.0878)-(startPos * 0.0878))/4096;
-  }
-  else
-  {
-    if((startPos == 0) && (endPos == 0))
-      multipler = 0.0878;
-    else if ((startPos > 0 ) && (endPos == 0))
-      multipler = ((360 * 0.0878) - (startPos * 0.0878)) / 4096;
-    else if ((startPos == 0 ) && (endPos > 0))
-      multipler = (endPos*0.0878) / 4096;
-    else if ((startPos > 0 ) && (endPos > 0))
-      multipler = ((endPos*0.0878)-(startPos * 0.0878))/ 4096;
-  }
-  return (newAngle * multipler);
-}
-
-/*******************************************************
-/* Function: burnAngle
-/* In: none
-/* Out: human readable string of success or failure
-/* Description: attempts to burn angle data to AMS5600
-/*******************************************************/
-String burnAngle()
-{
-  int burnResult = ams5600.burnAngle();
-  String returnStr = "Brun angle error: ";
-
-  switch (burnResult)
-  {
-    case 1:
-      returnStr = "Brun angle success";
-      break;
-    case -1:
-      returnStr += "no magnet detected";
-      break;
-    case -2:
-      returnStr += "no more burns left";
-      break;
-    case -3:
-      returnStr += "no positions set";
-      break;
-    default:
-      returnStr += "unknown";
-      break;
-  }
-  return returnStr;
-}
-
-/*******************************************************
-/* Function: burnMaxAngleAndConfig
-/* In: none
-/* Out: human readable string of sucess or failure
-/* Description: attempts to burn max angle and config data
-/* to AMS5600
-/*******************************************************/
-String burnMaxAngleAndConfig()
-{
-  int burnResult = ams5600.burnMaxAngleAndConfig();
-  String retStr = "Brun max angle and config error: ";
-
-  switch(burnResult)
-  {
-    case 1:
-      retStr = "Brun max angle and config success";
-      break;
-    case -1:
-      retStr += "chip has been burned once already";
-      break;
-    case -2:
-      retStr += "max angle less than 18 degrees";
-      break;
-    default:
-      retStr += "unknown";
-      break;
-  }
-  return retStr;
+  
+  // Now set up two tasks to run independently.
+  xTaskCreatePinnedToCore(
+    TaskBLEAdvertise
+    ,  "TaskBLEAdvertise"   // A name just for humans
+    ,  2048  // This stack size can be checked & adjusted by reading the Stack Highwater
+    ,  NULL
+    ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+    ,  NULL 
+    ,  ARDUINO_RUNNING_CORE);
 }
 
 void loop() {
-  
-  currentRawSegment = ams5600.getRawAngle();
-  if((previousRawSegment >= 0 && previousRawSegment < 256) && (currentRawSegment >= 3840 && currentRawSegment < 4096)) {
-    --segmentCounter;
-  } else if((previousRawSegment >= 3840 && previousRawSegment < 4096) && (currentRawSegment >= 0 && currentRawSegment < 256)) {
-    ++segmentCounter;
+  if(lastCout.length()>0) {
+    Serial.println(lastCout);
+    Serial.println("");
+    lastCout = "";
   }
-  previousRawSegment = currentRawSegment;
-  
-  if (SERIAL.available() > 0)
-  {
-    char incomingByteBuffer[5] = {0};
-    char incomingByte = 0;
-
-    incomingByteBuffer[0] = NULL;
-    incomingByteBuffer[1] = NULL;
-
-    SERIAL.readBytes(incomingByteBuffer,2);
-
-    if ((incomingByteBuffer[0] >= 48) && (incomingByteBuffer[0] < 60))
-    {
-      incomingByte = incomingByteBuffer[0] - 48;
-    }
-
-    if ((incomingByteBuffer[1] >= 48) && (incomingByteBuffer[1] < 60))
-    {
-      incomingByte *=10;
-      incomingByte += incomingByteBuffer[1] - 48;
-    }
-
-
-    switch (incomingByte)
-    {
-      case 5:
-      {
-        lastResponse = ("Start angle = "+String(convertRawAngleToDegrees(ams5600.getStartPosition()), DEC));
-      }
-      break;
-
-      case 6:
-      {
-        lastResponse = "End angle = " + String(convertRawAngleToDegrees(ams5600.getEndPosition()),DEC);
-      }
-      break;
-
-      case 7:
-      {
-        lastResponse = "Raw angle = "+ String(convertRawAngleToDegrees(ams5600.getRawAngle()),DEC);
-      }
-      break;
-
-      case 8:
-      {
-        lastResponse = "Scaled angle = "+String(convertScaledAngleToDegrees(ams5600.getScaledAngle()),DEC);
-      }
-      break;
-
-      case 11:
-      {
-        currentRawSegment = ams5600.getRawAngle();
-        pA.setRemainder(currentRawSegment);
-        pA.setSegment(segmentCounter);
-
-        lastResponse = "Point A - Current Segment: " + String(currentRawSegment) + "\n" + \
-                        "Point A - Segment Counter: " + String(segmentCounter);
-      }
-      break;
-
-      case 12:
-      {
-        currentRawSegment = ams5600.getRawAngle();
-        pB.setRemainder(currentRawSegment);
-        pB.setSegment(segmentCounter);
-
-        lastResponse = "Point B - Current Segment: " + String(currentRawSegment) + "\n" + \
-                        "Point B - Segment Counter: " + String(segmentCounter);
-      }
-      break;
-
-      case 13:
-      {
-        lastResponse = "Current Segment: " + String(currentRawSegment) + "\n" + \
-                        "Segment Counter: " + String(segmentCounter);
-      }
-      break;
-
-      default:
-      {
-          lastResponse = "Invalid Entry";
-      }
-      break;
-    }
-    /*end of menu processing */
-    printMenu();
-  }
-  delay(200);
 }
